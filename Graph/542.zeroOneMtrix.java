c755c310fdf7947621406eeeac3953ab3bfc56d0
dhanashri-25 <137794519+dhanashri-25@users.noreply.github.com>
2024-10-03 06:43:24 +0530

    shortest path in undirected graph

---
+++ Graph/542.zeroOneMtrix.java
@@ -0,0 +1,14 @@
+public class 542.zeroOneMtrix {
+  public int[][] nearest(int [][]grid){
+    int n=grid.length;
+    int m=grid[0].length;
+    int vis[][]=new int[n][m];
+    int dis[][]=new int [n][m];
+    Queue<Node>q=new LinkedList<>();
+    for(int i=0;i<n;i++){
+      for(int j=0;j<m;j++){
+        
+      }
+    }
+  }
+}
